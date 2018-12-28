a05d2afc6d1b0e86984ca7de2f8e1c321b1ce131
CareF <CareF.Lm@gmail.com>
2018-12-28 14:37:45 -0500

    init

---
+++ datetimewidget.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2011 ~ 2018 Deepin Technology Co., Ltd.
+ *
+ * Author:     sbw <sbw@sbw.so>
+ *
+ * Maintainer: sbw <sbw@sbw.so>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DATETIMEWIDGET_H
+#define DATETIMEWIDGET_H
+
+#include <QWidget>
+
+class DatetimeWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit DatetimeWidget(QWidget *parent = 0);
+
+    bool is24HourFormat() const { return m_24HourFormat; }
+
+signals:
+    void requestUpdateGeometry() const;
+    void requestContextMenu() const;
+
+public slots:
+    void set24HourFormat(const bool value);
+
+private:
+    QSize sizeHint() const;
+    void resizeEvent(QResizeEvent *e);
+    void paintEvent(QPaintEvent *e);
+    void mousePressEvent(QMouseEvent *e);
+
+    const QPixmap loadSvg(const QString &fileName, const QSize size);
+
+private:
+    QPixmap m_cachedIcon;
+    QString m_cachedTime;
+    bool m_24HourFormat;
+};
+
+#endif // DATETIMEWIDGET_H
